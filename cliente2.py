19d0dd9291afc1f1c19987697a1b8c3897fcf60e
Lucas Gabriel <lucasfacul20222@gmail.com>
2024-08-30 17:44:23 -0300

    feat: Adicionando compra utilizando grafos

---
+++ cliente2.py
@@ -0,0 +1,139 @@
+import socket
+import json
+import heapq
+
+def start_client():
+
+    # While do menu principal
+    while True:
+        print("Sistema de vendas\n")
+
+        escolha = (input("1- Comprar\n0- Encerrar programa\n>>> "))
+
+        while escolha != '1' and escolha != '0':
+            escolha = (input("Entrada inválida.\n\n1- Comprar\n0- Encerrar programa\n>>> "))
+
+        if escolha == '0':
+            break
+        
+        # Exibe cidades disponíveis
+        print("\n1- Cuiabá\n2- Goiânia\n3- Campo Grande\n4- Belo Horizonte\n5- Vitória")
+        print("6- São Paulo\n7- Rio de Janeiro\n8- Curitiba\n9- Florianópolis\n10- Porto Alegre\n")
+
+        # Se em algum momento escolhe sair, programa é encerrado e loop quebrado
+        # Se em algum momento escolhe menu, loop é reiniciado, voltando ao menu principal
+        print("0- Encerrar programa\n100- Menu\n")
+
+        while True:
+            origem = input("Escolha o número refetente a cidade origem: ")
+
+            if int(origem) < 0 or (int(origem) > 10 and int(origem) < 100) or int(origem) > 100:
+                origem = (input("Entrada inválida."))
+            
+            else:
+                break
+        
+        if origem == "0":
+            break
+        
+        if origem == "100":
+            continue
+
+        while True:
+            destino = input("Escolha o número refetente a cidade destino: ")
+
+            if int(destino) < 0 or (int(destino) > 10 and int(destino) < 100) or int(destino) > 100:
+                destino = (input("Entrada inválida."))
+            
+            else:
+                break
+        
+        if destino == "0":
+            break
+        
+        if destino == "100":
+            continue
+        
+        # Se não escolheu sair ou menu, conexão com server é iniciada para verificar voos disponíveis!!!!!!
+
+        # Cria um socket TCP/IP
+        # 1- Define ipv4
+        # 2- Socket do tipo TCP
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Conecta ao servidor
+        server_address = ('localhost', 65435)
+        client_socket.connect(server_address)
+
+        try:
+            mensagem = f"{origem},{destino}"
+            # Transforma cidade origem e destino em bytes e envia ao servidor
+            client_socket.sendall(mensagem.encode('utf-8'))
+
+            # Recebe a resposta do servidor (até 1024 bytes) -> Nesse caso seria uma lista de caminhos disponíveis
+            # Cada termo da lista é uma tupla contendo distancia em km e uma lista com os trechos
+            # Exemplo da lista com caminho de São Paulo até Vitória
+            # Ex: [ 
+            #       (22, ["São Paulo", "Rio de Janeiro", "Vitória"]), -> Lista[0] tem tudo isso
+            #       (30, ["São Paulo", "Vitória"])                    -> Lista[1] tem tudo isso
+            #     ]
+            data = client_socket.recv(1024)
+
+            # Transforma em lista novamente
+            caminhos = json.loads(data.decode('utf-8'))
+
+            # Listando os caminhos do menor para o maior
+            if caminhos:
+                cidades = ["Cuiaba", "Goiania", "Campo Grande", "Belo Horizonte", "Vitoria", 
+                       "Sao Paulo", "Rio de Janeiro", "Curitiba", "Florianopolis", "Porto Alegre"]
+                
+                print(f"Voos de {cidades[int(origem)-1]} para {cidades[int(destino)-1]}\n")
+                i = 1
+
+                tam = len(caminhos)
+                while caminhos:
+                    # Pega o primeiro item da lista, remove ele da lista e atualiza lista
+                    dist, path = heapq.heappop(caminhos)
+                    print(f"{i}. Caminho: {' -> '.join(path)} | {dist}km\n")
+                    i+=1
+
+                print("0- Encerrar programa\n100- Menu\n")
+
+                while True:
+                    escolha = input("Escolha um caminho: ")
+
+                    if (int(escolha) < 0 or int(escolha) > 100 or (int(escolha) > tam and int(escolha) <= 99)):
+                        escolha = (input("Entrada inválida."))
+                    
+                    else:
+                        break
+
+                if escolha == "0":
+                    break
+                
+                if escolha == "100":
+                    continue
+                
+                id = input("Digite seu CPF para registro da compra (apenas os números): ")
+
+                mensagem = f"{escolha},{id}"
+
+                # Se escolheu um caminho, envia para o server e compra seu assento
+                client_socket.sendall(mensagem.encode('utf-8'))
+
+                # Recebe a resposta do servidor (até 1024 bytes).
+                data = client_socket.recv(1024)
+                print(f"{data.decode('utf-8')}")
+
+            # Se a lista veio vazia, não tem caminhos, volta ao menu        
+            else:
+                print(f"Nenhum caminho disponível de {origem} para {destino}\n\n")
+
+        finally:
+            # Fecha a conexão
+            client_socket.close()
+    
+    print("Até a próxima\n")
+
+if __name__ == "__main__":
+    start_client()
---
+++ servidor2.py
@@ -0,0 +1,223 @@
+import socket
+
+import networkx as nx
+import heapq
+import json
+
+def carregar_grafo(arquivo):
+    # Abre arquivo e retorna os dados do grafo
+    with open(arquivo, 'r') as arq:
+        dados_existentes = json.load(arq)
+    
+    # Cria novo grafo para armazenar dados do grafo retornado
+    grafo = nx.DiGraph()
+
+    # Salva cada trecho no grafo
+    for trecho in dados_existentes['trecho']:
+        grafo.add_edge(
+            trecho['v1'], trecho['v2'], 
+            distancia = trecho['distancia'], 
+            assentos = trecho['assentos'],
+            id = trecho['id']
+        )
+    
+    return grafo
+
+
+def salvar_grafo(grafo, arquivo):
+
+    # Novo grafo a ser salvo em arquivo
+    dados_novos = {'trecho': []}
+
+    # Salva novos dados de cada trecho (atualiza na lista de cada trecho. LISTA = informações como distancia e etc)
+    for v1, v2, atributos in grafo.edges(data=True):
+        dados_novos['trecho'].append({
+            'v1': v1,
+            'v2': v2,
+            'distancia': atributos['distancia'],
+            'assentos': atributos['assentos'],
+            'id': atributos['id']
+        })
+
+    # Salva novo grafo em arquivo
+    with open(arquivo, 'w') as arq:
+        json.dump(dados_novos, arq, indent=4)
+
+
+def encontrar_caminhos(grafo, cidade_inicial, cidade_fim):
+    caminhos = []
+    
+    # Retorna todos os caminhos possíveis entre v1 e v2
+    for path in nx.all_simple_paths(grafo, source=cidade_inicial, target=cidade_fim):
+
+        # Verifica se algum trecho do caminho encontrado não possui assentos
+        caminho_valido = True
+        for i in range(len(path) - 1):
+            trecho = (path[i], path[i + 1])
+            if grafo[trecho[0]][trecho[1]]['assentos'] == 0:
+                caminho_valido = False
+                break
+        
+        if caminho_valido:
+            # Calcula distancia do caminho encontrado
+            dist = sum(grafo[path[i]][path[i + 1]]['distancia'] for i in range(len(path) - 1))
+
+            # Organiza os caminhos em fila de prioridade (menor ao maior) a depender da distancia
+            heapq.heappush(caminhos, (dist, path))
+    
+    return caminhos
+
+def start_server():
+
+    # Arquivo JSON onde o grafo será salvo/carregado
+    arquivo_grafo = 'grafo.json'
+
+    # Carregando o grafo a partir do arquivo JSON
+    try:
+        G = carregar_grafo(arquivo_grafo)
+        print("Grafo carregado com sucesso.")
+    
+    except FileNotFoundError:
+        print("Arquivo não encontrado. Criando um novo grafo.")
+        
+        # Criando um novo grafo com 5 vértices (cidades) e salvando
+        G = nx.DiGraph()
+
+        # Caminhos de Cuiaba
+        G.add_edge("Cuiaba", "Goiania", distancia=890, assentos=3, id=[])
+        G.add_edge("Cuiaba", "Campo Grande", distancia=700, assentos=3, id=[])
+
+        # Caminhos de Goiania
+        G.add_edge("Goiania", "Cuiaba", distancia=890, assentos=3, id=[])
+        G.add_edge("Goiania", "Campo Grande", distancia=840, assentos=3, id=[])
+        G.add_edge("Goiania", "Belo Horizonte", distancia=890, assentos=3, id=[])
+
+        # Caminhos de Campo Grande
+        G.add_edge("Campo Grande", "Cuiaba", distancia=700, assentos=3, id=[])
+        G.add_edge("Campo Grande", "Goiania", distancia=840, assentos=3, id=[])
+        G.add_edge("Campo Grande", "Belo Horizonte", distancia=1250, assentos=3, id=[])
+        G.add_edge("Campo Grande", "Sao Paulo", distancia=980, assentos=3, id=[])
+        G.add_edge("Campo Grande", "Curitiba", distancia=1000, assentos=3, id=[])
+
+        # Caminhos de Belo Horizonte
+        G.add_edge("Belo Horizonte", "Goiania", distancia=890, assentos=3, id=[])
+        G.add_edge("Belo Horizonte", "Vitoria", distancia=510, assentos=3, id=[])
+        G.add_edge("Belo Horizonte", "Campo Grande", distancia=1250, assentos=3, id=[])
+        G.add_edge("Belo Horizonte", "Sao Paulo", distancia=585, assentos=3, id=[])
+        G.add_edge("Belo Horizonte", "Rio de Janeiro", distancia=440, assentos=3, id=[])
+
+        # Caminhos de Vitoria
+        G.add_edge("Vitoria", "Belo Horizonte", distancia=510, assentos=3, id=[])
+        G.add_edge("Vitoria", "Rio de Janeiro", distancia=520, assentos=3, id=[])
+
+        # Caminhos de Sao Paulo
+        G.add_edge("Sao Paulo", "Belo Horizonte", distancia=585, assentos=3, id=[])
+        G.add_edge("Sao Paulo", "Campo Grande", distancia=980, assentos=3, id=[])
+        G.add_edge("Sao Paulo", "Rio de Janeiro", distancia=440, assentos=3, id=[])
+        G.add_edge("Sao Paulo", "Curitiba", distancia=400, assentos=3, id=[])
+
+        # Caminhos de Rio de Janeiro
+        G.add_edge("Rio de Janeiro", "Vitoria", distancia=520, assentos=3, id=[])
+        G.add_edge("Rio de Janeiro", "Belo Horizonte", distancia=440, assentos=3, id=[])
+        G.add_edge("Rio de Janeiro", "Sao Paulo", distancia=440, assentos=3, id=[])
+
+        # Caminhos de Curitiba
+        G.add_edge("Curitiba", "Campo Grande", distancia=1000, assentos=3, id=[])
+        G.add_edge("Curitiba", "Sao Paulo", distancia=400, assentos=3, id=[])
+        G.add_edge("Curitiba", "Florianopolis", distancia=300, assentos=3, id=[])
+
+        # Caminhos de Florianopolis
+        G.add_edge("Florianopolis", "Curitiba", distancia=300, assentos=3, id=[])
+        G.add_edge("Florianopolis", "Porto Alegre", distancia=460, assentos=3, id=[])
+
+        # Caminhos de Porto Alegre
+        G.add_edge("Porto Alegre", "Florianopolis", distancia=460, assentos=3, id=[])
+
+        # Estrutura é parecida com isso
+        # { 
+        #   ("São Paulo", "Rio de Janeiro") : {'distancia': 980, 'assentos': 3, 'id': []}
+        #   ("Vitória", "Curitiba") : {'distancia': 34, 'assentos': 3, 'id': []}
+        #   ...
+        # }
+
+        # É um dicionário geral que armazena todas as conexões (grafo)
+        # As chaves desse dicionário são tuplas que armazenam a cidade inicio e a cidade fim
+        # Os valores dessas chaves são dicionários, onde as chaves serão as distancias, assentos e id. Os valores
+        # dessas chaves são as informações desses dados
+
+        salvar_grafo(G, arquivo_grafo)
+
+    # Cria um socket TCP/IP
+    # 1- Define ipv4
+    # 2- Socket do tipo TCP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Liga o socket a um endereço e porta
+    server_address = ('localhost', 65435)
+    server_socket.bind(server_address)
+
+    # Servidor aguarda por conexões
+    server_socket.listen(1)
+    print("Aguardando conexão...\n")
+
+    while True:
+        # Espera por uma conexão
+        # Bloqueia a execução do programa até alguém se conectar
+        # Retorna socket do cliente e seu endereço IP
+        connection, client_address = server_socket.accept()
+        
+        try:
+            print(f"Conectado a {client_address}")
+
+            # Cidade origem e destino
+            data1 = connection.recv(1024)
+
+            if data1:
+                cidades = ["Cuiaba", "Goiania", "Campo Grande", "Belo Horizonte", "Vitoria", 
+                        "Sao Paulo", "Rio de Janeiro", "Curitiba", "Florianopolis", "Porto Alegre"]
+                
+                origem, destino = data1.decode('utf-8').split(',')
+
+                origem = cidades[int(origem)-1]
+                destino = cidades[int(destino)-1]
+
+                print(f"Recebido a origem: {origem}")
+                print(f"Recebido o destino: {destino}")
+            
+                # Verifica caminhos
+                caminhos = encontrar_caminhos(G, origem, destino)
+
+                # Transforma lista e envia para o cliente
+                serializa = json.dumps(caminhos)
+                connection.sendall(serializa.encode('utf-8'))
+
+                if caminhos:
+                    # Espera escolha do cliente
+                    data3 = connection.recv(1024)
+
+                    if data3:
+                        indice, cpf = data3.decode('utf-8').split(',')
+                        print(f"Recebido {indice}, {cpf}")
+
+                        # Escolha do caminho pelo cliente
+                        dist, path = caminhos[int(indice) - 1]
+                        
+                        # Remove um asssento de determinado trecho no grafo
+                        # Adiciona um ID ao assento comprado
+                        for i in range(len(path) - 1):
+                            trecho = (path[i], path[i + 1])
+                            G[trecho[0]][trecho[1]]['assentos'] -= 1
+                            G[trecho[0]][trecho[1]]['id'].append(cpf)
+                        
+                        salvar_grafo(G, arquivo_grafo)
+
+                        message = "\nCompra feita com sucesso\n"                    
+                        connection.sendall(message.encode('utf-8'))
+
+        finally:
+            # Fecha a conexão
+            connection.close()
+            print("Conexão encerrada. Aguardando nova conexão...\n")
+
+if __name__ == "__main__":
+    start_server()
