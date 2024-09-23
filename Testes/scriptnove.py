ecaf86aa2611b2883284a08e8aa10b084d177ca5
icaroo-oliveira <143228771+icaroo-oliveira@users.noreply.github.com>
2024-09-23 13:43:21 -0300

    test: script para multilos clientes

--- Testes/scriptnove.py
+++ Testes/scriptnove.py
@@ -2,25 +2,32 @@ import subprocess
 import time
 import threading
 
-client_script = "client.py"
+client_script = "cliente_teste.py"
+#se liga em cima, se der erro puxa pra fora o arquivo pra fora da pasta teste (se ele tiver dentro)
+cont=111111111111111
 
-escolhas = ['1', '1', '2', '1', '213213213']
 
 def abrir_terminal(escolhas):
     escolha_str = ' '.join(escolhas)
-    comando = f'gnome-terminal -- bash -c "python3 {client_script} {escolha_str}; exec bash"'
-    
+    #comando = f'gnome-terminal -- bash -c "python3 {client_script} {escolha_str}; exec bash"'
+    comando = f'start cmd /k python {client_script} {escolha_str}'
+
     print(f"Executando comando: {comando}")
 
     # Executa o comando no sistema
     subprocess.Popen(comando, shell=True)
 
 # Número de clientes/terminais
-num_clients = 5
+num_clients = 4
 
 # Abrindo múltiplos terminais com os mesmos parâmetros
 for i in range(num_clients):
-    client_thread = threading.Thread(target=abrir_terminal, args=(escolhas,))
-    client_thread.start()
+    
+    escolhas = ['1', '1', '2', '1', str(cont)]
+
+    #client_thread = threading.Thread(target=abrir_terminal, args=(escolhas,))
+    #client_thread.start()
+    abrir_terminal(escolhas)
+    cont+=1
     
    
